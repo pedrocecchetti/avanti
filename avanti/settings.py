d2f6d375eaba7c3f91bb9c1a53962b4f614fb905
Pedro Cecchetti <pedro.cecchetti@gmail.com>
2021-09-07 23:08:10 +0200

    feat: added Template folder and updated Secret

--- avanti/settings.py
+++ avanti/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vy7-d(wr^7a)72#$roo6=c=f=hbxdnw6(y3k#(ab$h1+20070k'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-vy7-d(wr^7a)72#$roo6=c=f=hbxdnw6(y3k#(ab$h1+20070k')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'avanti.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['apps/web/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
