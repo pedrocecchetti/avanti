e62f866428a27bf59d545ef5f6015c0b8a07b013
Pedro Cecchetti <pedro.cecchetti@gmail.com>
2022-02-14 23:21:30 +0100

    feat: created better design

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -1,7 +1,8 @@
-from apps.web.views import HomeView
+from apps.web.views import HomeView, receive_contact_information
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('', HomeView.as_view()),
+    path('contact/', receive_contact_information)
 ]
