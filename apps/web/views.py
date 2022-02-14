8b645feb464f662e33b16093e5a3afb2ca85fa02
Pedro Cecchetti <pedro.cecchetti@gmail.com>
2022-02-14 14:08:30 +0100

    refactor: reorganized templates

--- apps/web/views.py
+++ apps/web/views.py
@@ -5,3 +5,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "home.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        header_classes = {"home": "current", "right_sidebar": "current", "dropdown": "current"}
+        context['header_classes'] = header_classes
+        return context
